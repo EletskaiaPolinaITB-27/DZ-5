506c5d98617e884b139b89733c1b81b9ef8d3daa
Polina Eletskaia <polina.e@compsscollege.art>
2026-02-14 13:44:30 +0600

    add

---
+++ DZ-5-movie/src/components/AddMovie/AddMovie.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+interface AddMovieProps {
+  onAddMovie: (title: string) => void
+}
+
+export const AddMovie: React.FC<AddMovieProps> = ({ onAddMovie }) => {
+  const [movieValue, setMovieValue] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMovieValue(e.target.value)
+  }
+
+  const submitMovie = () => {
+    const trimmedValue = movieValue.trim()
+    if (!trimmedValue) return
+    onAddMovie(trimmedValue)
+    setMovieValue('')
+  }
+
+  return (
+    <div className="add-container">
+      <input
+        type="text"
+        placeholder="Write film"
+        value={movieValue}
+        onChange={handleChange}
+      />
+      <button onClick={submitMovie}>Add</button>
+    </div>
+  )
+}
